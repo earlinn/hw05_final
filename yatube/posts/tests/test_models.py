01db950299aedc201e997573297dd8982bf035db
Galina Volkova <earlinndrow@gmail.com>
2022-02-07 14:27:58 +0300

    Refactor test_models of the posts app

--- yatube/posts/tests/test_models.py
+++ yatube/posts/tests/test_models.py
@@ -29,70 +29,58 @@ class PostModelTest(TestCase):
             author=cls.user,
             text=TEST_COMMENT_TEXT,
         )
-
-    def test_post_have_correct_object_name(self):
-        """У модели Post корректно работает __str__."""
-        post = PostModelTest.post
-        expected_post_name = post.text[:15]
-        self.assertEqual(expected_post_name, str(post))
-
-    def test_group_have_correct_object_name(self):
-        """У модели Group корректно работает __str__."""
-        group = PostModelTest.group
-        expected_group_name = group.title
-        self.assertEqual(expected_group_name, str(group))
-
-    def test_comment_have_correct_object_name(self):
-        """У модели Comment корректно работает __str__."""
-        comment = PostModelTest.comment
-        expected_comment_name = comment.text
-        self.assertEqual(expected_comment_name, str(comment))
-
-    def test_post_verbose_name(self):
-        """Verbose_name в полях модели Post совпадает с ожидаемым."""
-        post = PostModelTest.post
-        field_verboses = {
-            'text': 'Текст поста',
-            'pub_date': 'Дата публикации',
-            'author': 'Автор',
-            'group': 'Группа',
-            'image': 'Картинка',
+        cls.object_as_string = {
+            cls.post: cls.post.text[:15],
+            cls.group: cls.group.title,
+            cls.comment: cls.comment.text,
         }
-        for value, expected in field_verboses.items():
-            with self.subTest(value=value):
-                self.assertEqual(
-                    post._meta.get_field(value).verbose_name, expected)
-
-    def test_comment_verbose_name(self):
-        """Verbose_name в полях модели Comment совпадает с ожидаемым."""
-        comment = PostModelTest.comment
-        field_verboses = {
-            'post': 'Пост',
-            'author': 'Автор комментария',
-            'text': 'Текст комментария',
-            'created': 'Дата создания',
+        cls.field_verboses = {
+            cls.post: {
+                'text': 'Текст поста',
+                'pub_date': 'Дата публикации',
+                'author': 'Автор',
+                'group': 'Группа',
+                'image': 'Картинка',
+            },
+            cls.comment: {
+                'post': 'Пост',
+                'author': 'Автор комментария',
+                'text': 'Текст комментария',
+                'created': 'Дата создания',
+            },
         }
-        for value, expected in field_verboses.items():
-            with self.subTest(value=value):
-                self.assertEqual(
-                    comment._meta.get_field(value).verbose_name, expected)
-
-    def test_post_help_text(self):
-        """Help_text в полях модели Post совпадает с ожидаемым."""
-        post = PostModelTest.post
-        field_help_texts = {
-            'text': 'Текст нового поста',
-            'group': 'Группа, к которой будет относиться пост'
+        cls.field_help_texts = {
+            cls.post: {
+                'text': 'Текст нового поста',
+                'group': 'Группа, к которой будет относиться пост',
+            },
+            cls.comment: {
+                'text': 'Введите текст комментария',
+            },
         }
-        for value, expected in field_help_texts.items():
-            with self.subTest(value=value):
-                self.assertEqual(
-                    post._meta.get_field(value).help_text, expected)
 
-    def test_comment_help_text(self):
-        """Help_text в полях модели Comment совпадает с ожидаемым."""
-        comment = PostModelTest.comment
-        self.assertEqual(
-            comment._meta.get_field('text').help_text,
-            'Введите текст комментария'
-        )
+    def test_models_have_correct_object_names(self):
+        """У моделей приложения posts корректно работает __str__."""
+        for object, string in PostModelTest.object_as_string.items():
+            with self.subTest(object=object):
+                self.assertEqual(str(object), string)
+
+    def test_models_verbose_names(self):
+        """Verbose name в полях моделей совпадают с ожидаемым."""
+        for object, verboses_dict in PostModelTest.field_verboses.items():
+            for field, verbose_name in verboses_dict.items():
+                with self.subTest(field=field):
+                    self.assertEqual(
+                        object._meta.get_field(field).verbose_name,
+                        verbose_name
+                    )
+
+    def test_models_help_texts(self):
+        """Help text в полях моделей совпадают с ожидаемым."""
+        for object, help_text_dict in PostModelTest.field_help_texts.items():
+            for field, help_text in help_text_dict.items():
+                with self.subTest(field=field):
+                    self.assertEqual(
+                        object._meta.get_field(field).help_text,
+                        help_text
+                    )
